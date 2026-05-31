fd0990848fc1dc6ae989c76e770d5399ff5cfb52
Stanislav22401 <stanislav2240@gmail.com>
2026-05-31 19:36:49 +0300

    Polynom changes

--- lab02/crypto/variant.js
+++ lab02/crypto/variant.js
@@ -3,11 +3,11 @@
 export const VARIANT = 7;
 export const REGISTER_LENGTH = 29;
 
-/** Примитивный многочлен: x^29 + x^27 + 1 (стандартная таблица PN для степени 29) */
-export const POLYNOMIAL_LABEL = 'x^29 + x^27 + 1';
+/** Примитивный многочлен: x^29 + x^2 + 1 */
+export const POLYNOMIAL_LABEL = 'x^29 + x^2 + 1';
 
 /**
  * Отводы для Galois LFSR (степени < n, кроме x^n).
- * Обратная связь: XOR ячеек с индексами 0 и 27.
+ * Обратная связь: XOR ячеек с индексами 0 и 2.
  */
-export const FEEDBACK_TAPS = [0, 27];
+export const FEEDBACK_TAPS = [0, 2];
